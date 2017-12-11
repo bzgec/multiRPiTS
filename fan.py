afc99fb226a3bab3a514f0c072a12317c411b8c4
bzgec <34318921+bzgec@users.noreply.github.com>
2017-12-11 19:12:42 +0000

    Add files via upload

---
+++ fan.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+import os
+from time import sleep
+import sys
+import RPi.GPIO as GPIO
+
+fanPin = 17
+desiredTmp = 45
+class PIDs:
+	p = 40
+	i = 15
+	d = 30
+	integral = 0
+	delta_t = 3
+	previousValue = desiredTmp
+
+def getCpuTmp():
+	tmpLine = os.popen('vcgencmd measure_temp').readline()
+	tmp = (tmpLine.replace('temp=','').replace("'C\n",""))
+	#print('CPU temperature: {0}'.format(tmp))
+	return tmp
+def PIDcontroller(realValue, setPoint):
+	print('realValue: ' + str(realValue))
+	print('setPoint: ' + str(setPoint))
+	error = realValue - setPoint
+	print('error: ' + str(error))
+	P = PIDs.p*error
+	print('P: ' + str(P))
+	PIDs.integral += error
+	I = PIDs.i*PIDs.integral
+	print('I: ' + str(I))
+	derivate = (PIDs.previousValue - realValue)/PIDs.delta_t
+	PIDs.previousValue = realValue
+	D = PIDs.d*derivate
+	print('D: ' + str(D))
+	print('total: ' + str(P+I+D))
+	return P + I + D
+def fan(command):
+	if command == 'on':
+		GPIO.output(fanPin, 1)
+		print('fan: ON')
+	else:
+		GPIO.output(fanPin,0)
+		print('fan: OFF')
+	return()
+def handleFan():
+	cpuTmp = float(getCpuTmp())
+	dutyCycle = PIDcontroller(cpuTmp, desiredTmp)
+	
+	if dutyCycle > 100:
+		dutyCycle = 100
+	elif dutyCycle < 20:
+		dutyCycle = 0
+	#if dutyCycle == 100 or dutyCycle == 0:
+	#	if dutyCycle == 100:
+	#		fan('on')
+	#	else:
+	#		fan('off')
+	#else:
+	pwmFanPin.ChangeDutyCycle(dutyCycle)
+	if dutyCycle == 0:
+		fan('off')
+	elif dutyCycle == 100:
+		fan('on')
+	print('dutyCycle: ' + str(dutyCycle))
+	print('')
+	return()
+try:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(fanPin,GPIO.OUT)
+	pwmFanPin = GPIO.PWM(fanPin,50)	# frequency is 50Hz
+	pwmFanPin.start(100)	# duty cicle is 50%
+	while True:
+		handleFan()
+		sleep(PIDs.delta_t)	# goes to sleep for delta_t
+except KeyboardInterrupt:	#CTRL+C = keyboard interrupt
+	pwmFanPin.ChangeDutyCycle(0)
+	print('KeyboardInterrupt')
+	GPIO.cleanup() #resets all GPIO ports used by this program
